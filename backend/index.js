ca6e3a3a8b78cdeb424e10feb0fc41546149219f
unknown <teufikjonas@gmail.com>
2025-05-22 00:09:26 +0700

    Add .gitignore
    git push origin main

--- backend/index.js
+++ backend/index.js
@@ -7,10 +7,31 @@ const PORT = process.env.PORT || 4000;
 
 
 // for pass N3tLAB123p@ss
+//N3tLAB100p@ss
+//for pass T3st1234
+// pass for sbd = SbD@pass
+// p@SSw0rd14PASS
 
 // Middleware
 const frontendUrl = process.env.FRONTEND_URL || 'http://localhost:5173';
-app.use(cors({ origin: frontendUrl })); // Tambahkan middleware CORS
+const allowedOrigins = [
+  'http://localhost',
+  'http://localhost:80',
+  'http://localhost:5173'
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    // Allow requests with no origin (like mobile apps, curl, etc.)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
